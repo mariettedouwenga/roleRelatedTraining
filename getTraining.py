801ee42fcdd44d6dde2ff07e244f2687a544810b
Douwenga <mariette.douwenga@boskalis.com>
2025-03-07 08:38:51 +0100

    changed to one col and added button to training page

--- getTraining.py
+++ getTraining.py
@@ -6,14 +6,15 @@ import markdown
 header = st.container()
 dataset = st.container()
 trainingOutput = st.container()
-col1, col2 = st.columns(2)
+#col1, col2= st.columns(2, gap="small", border=True)
+col1= st.columns(1, border=True)
 
 # load excel with its path 
 trainingMatrix = openpyxl.load_workbook("TrainingMatrix.xlsx") 
-trainingDates = openpyxl.load_workbook("trainingDates.xlsx")
+trainingNames = openpyxl.load_workbook("trainingNames.xlsx")
 
 trainingMatrix_sh = trainingMatrix.active
-trainingDates_sh = trainingDates.active
+trainingNames_sh = trainingNames.active
 
 with header:
     st.title('Infor LN Role Related Training')
@@ -22,10 +23,11 @@ with header:
     st.subheader('Find the training related to your Infor LN role.')
     roleData = pd.read_csv('inforRoles.csv')
     #st.write(roleData.head()) 
+    trainingNames = pd.read_excel('trainingNames.xlsx')
 
-with col1:
+#with col1:
     role = st.selectbox('Select a role', roleData, index = None,
-    placeholder="Select your Infor Ln role",)
+    placeholder="Select your Infor LN role",)
       
 # iterate through excel and display data 
 
@@ -43,14 +45,40 @@ with col1:
             if cell_obj.value == "x":
                 if role_name.value == role:                                                         
                                                 
-                            st.write("Training name: " + f"**{training_name.value}**")
-                            st.text("Description: " + training_description.value)
-                            st.text("Dates: " + "\n" + str(training_date.value))
-                            st.divider()
+                    st.write(f"**{training_name.value}**")
+                    #st.write(f"**{training_name.value}**")
+                    st.text("Description: " + "\n" + training_description.value)
+                    st.link_button("Find training dates", "https://boskalis.sharepoint.com/sites/D001418/SitePages/Training-Dates.aspx#register-for-training")
+                    st.divider()
     
-with col2:
-    trainingDatesData = st.selectbox('Select date', trainingMatrix_sh.values, index = None,
-    placeholder="Select date",)
+# with col2:
+#     trnName = st.selectbox('Register for training', trainingNames, index = None,
+#     placeholder="Select training name",)
+   
+    
+#     df = pd.DataFrame(trainingNames)
+#     #st.write(trainingNames.head()) 
+#     #df.loc[:,"January 2025" : "December 2025"]
+#     #df.iloc[1:2, 1:13]
+
+#     for i in range(1, trainingNames_sh.max_row+1):
+
+#         for j in range(1, trainingNames_sh.max_column+1): 
+        
+#             cell_objName = trainingNames_sh.cell(i, column=1) #1,1; 2,1; 3,1
+#             Dates = trainingNames_sh.cell(i,column=j + 1) #1,2; 2,3; 3;4
+                                
+#             if cell_objName.value == trnName:                                                     
+
+#                 with col2:                                        
+#                     st.text("Available Date: " + "\n" + str(Dates.value))  
+#                     st.divider()
+
+        
+
+
+
+               
 
     
 
